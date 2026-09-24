ce6abdb3a7ceca4ded7bf154dacade5ac398bceb
labdashs <filipsamul48@gmail.com>
2026-09-24 15:09:04 +0100

    Add files via upload

---
+++ questionare.py
@@ -0,0 +1,8 @@
+print("QUESTIONARE")
+user_input = input("Is 67 skibidi goated yes or no? ")
+if user_input.lower() == "yes":
+    print("yup right answer")
+else:
+    print("wrong answer")
+    import webbrowser
+    webbrowser.open("www.pornhub.com")
